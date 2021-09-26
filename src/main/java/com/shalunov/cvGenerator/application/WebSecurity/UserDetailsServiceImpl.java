0d1c90e5c445834cc3df26c9ffb105ee55637ae4
Dmitriy <schalunov002@gmail.com>
2021-09-26 03:25:48 +0300

    Configure jwt auth

---
+++ src/main/java/com/shalunov/cvGenerator/application/WebSecurity/UserDetailsServiceImpl.java
@@ -0,0 +1,30 @@
+package com.shalunov.cvGenerator.application.WebSecurity;
+
+import com.shalunov.cvGenerator.domain.User;
+import com.shalunov.cvGenerator.infrastructure.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String username)
+            throws UsernameNotFoundException {
+
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() ->
+                        new UsernameNotFoundException("User Not Found with -> username or email : " + username)
+                );
+
+        return UserPrinciple.build(user);
+    }
+}
